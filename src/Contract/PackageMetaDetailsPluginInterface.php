1a1eb73e461a2c5aa454aaa221e5f1787133cf00
Andrew Dawes <andrewjamesdawes@gmail.com>
2025-08-31 13:09:10 -0400

    Remove getPluginFile

--- src/Contract/PackageMetaDetailsPluginInterface.php
+++ src/Contract/PackageMetaDetailsPluginInterface.php
@@ -4,7 +4,6 @@ namespace CodeKaizen\WPPackageMetaProviderContract\Contract;
 
 interface PackageMetaDetailsPluginInterface extends PackageMetaDetailsInterface
 {
-    public function getPluginFile(): string;
     public function getSections(): array;
     /** @return array<string,string> */
     public function getNetwork(): bool;
