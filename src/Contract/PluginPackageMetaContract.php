b16ce19cb2a229dc9f03690d62e6f960ad9d383a
Andrew Dawes <andrewjamesdawes@gmail.com>
2025-09-27 12:46:55 -0400

    Fix contract

--- src/Contract/PluginPackageMetaContract.php
+++ src/Contract/PluginPackageMetaContract.php
@@ -4,8 +4,8 @@ namespace CodeKaizen\WPPackageMetaProviderContract\Contract;
 
 interface PluginPackageMetaContract extends PackageMetaContract
 {
-    public function getSections(): array;
     /** @return array<string,string> */
+    public function getSections(): array;
     public function getNetwork(): bool;
     /** @return string[] */
     public function getRequiresPlugins(): array;
